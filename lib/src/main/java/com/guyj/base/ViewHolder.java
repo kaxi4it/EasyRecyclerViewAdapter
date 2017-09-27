2b8fcea9d99b25563affcf872e81f6a09386a587
顾叶菁 <gyj_android@sina.com>
2017-09-27 18:33:04 +0800

    Update ViewHolder.java
    ViewHolder中添加了隐藏item的方法，可以再避免修改数据源集合的情况下，隐藏列表显示内容，position会相应保留

--- lib/src/main/java/com/guyj/base/ViewHolder.java
+++ lib/src/main/java/com/guyj/base/ViewHolder.java
@@ -134,6 +134,24 @@ public class ViewHolder extends RecyclerView.ViewHolder
 
 
     /****以下为辅助方法*****/
+    
+    /**
+     * 设置ItemView是否显示，true显示，false隐藏
+     * @param isVisible
+     */
+    public void setItemViewVisibility(boolean isVisible){
+        RecyclerView.LayoutParams param = (RecyclerView.LayoutParams)itemView.getLayoutParams();
+        if (isVisible){
+            param.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            param.width = LinearLayout.LayoutParams.MATCH_PARENT;
+            itemView.setVisibility(View.VISIBLE);
+        }else{
+            itemView.setVisibility(View.GONE);
+            param.height = 0;
+            param.width = 0;
+        }
+        itemView.setLayoutParams(param);
+    }
 
     /**
      * 设置TextView的值
